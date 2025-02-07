d4d14a5941df0db8ca4955751b666f474d7dd3c2
Toluhunter <toluhunterdev@gmail.com>
2025-02-08 00:10:45 +0100

    setup train.py imports

---
+++ train.py
@@ -0,0 +1,16 @@
+import re
+import numpy as np # linear algebra
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.base import BaseEstimator, TransformerMixin
+from datetime import datetime
+from sklearn.metrics import f1_score
+from wordcloud import WordCloud
